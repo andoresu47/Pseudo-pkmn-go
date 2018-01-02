988e29943d0fcec1f11ce6b78784d12db86f4d82
Andres Lopez Martinez <andoresu@ciencias.unam.mx>
2018-01-02 14:07:27 -0600

    Generalized schema in order to execute it once and get everything ready (db-wise).

--- src/db_stuff/schema.sql
+++ src/db_stuff/schema.sql
@@ -1,3 +1,13 @@
+DROP OWNED BY redes;
+DROP DATABASE IF EXISTS proyecto2redes;
+DROP ROLE IF EXISTS redes;
+
+create role redes with login password 'redes1234';
+create database proyecto2redes owner redes;
+grant all privileges on database proyecto2redes to redes;
+
+\c proyecto2redes
+
 -- Tabla de Pokemon
 CREATE TABLE Pokemon(
 	nIdpokemon	SERIAL						,
@@ -24,3 +34,7 @@ CREATE TABLE Captura(
     CONSTRAINT fk_captura_pokemon FOREIGN KEY(nIdpokemon) REFERENCES Pokemon(nIdpokemon) ON DELETE CASCADE,
     CONSTRAINT fk_captura_usuario FOREIGN KEY(nIdusuario) REFERENCES Usuario(nIdusuario) ON DELETE CASCADE
 );
+
+ALTER TABLE Pokemon OWNER TO redes;
+ALTER TABLE Usuario OWNER TO redes;
+ALTER TABLE Captura OWNER TO redes;
