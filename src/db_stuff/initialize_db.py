47e054fd9eefdaccc0a4ede30d3a39a40dd2f8e2
Andres Lopez Martinez <andoresu@ciencias.unam.mx>
2018-01-02 14:15:48 -0600

    Script to initialize DB with 7 distinct users and the 151 first-generation pokemon.

---
+++ src/db_stuff/initialize_db.py
@@ -0,0 +1,11 @@
+import src.controller.controller_functions as cf
+
+if __name__ == '__main__':
+    cf.populate_pokemon()
+    cf.register_user("redes1", "pass1")
+    cf.register_user("redes2", "pass2")
+    cf.register_user("redes3", "pass3")
+    cf.register_user("redes4", "pass4")
+    cf.register_user("redes5", "pass5")
+    cf.register_user("redes6", "pass6")
+    cf.register_user("redes7", "pass7")
